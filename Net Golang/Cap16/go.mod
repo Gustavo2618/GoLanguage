4f24fafef5ba83f71a146ff779dbbe138bbaa070
Gustavo <luizg473@gmail.com>
2025-08-21 11:35:44 -0300

    add cap 16

---
+++ Net Golang/Cap16/go.mod
@@ -0,0 +1,5 @@
+module cap16
+
+go 1.24.2
+
+require golang.org/x/crypto v0.41.0
--- Net Golang/Cap16/main.go
+++ Net Golang/Cap16/main.go
@@ -4,6 +4,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"sort"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 func main() {
@@ -11,6 +14,9 @@ func main() {
 	exemplo02()
 	exemplo03()
 	exemplo04()
+	exemplo05()
+	exemplo06()
+	exemplo07()
 }
 
 type ColorGroup struct {
@@ -91,4 +97,109 @@ func exemplo04() {
 	fmt.Println()
 	encoder := json.NewEncoder(os.Stdout)
 	encoder.Encode(darth)
+}
+
+// interface writer pacote sort video 116,117,118
+func exemplo05() {
+	ss := []string{"mofi", "total", "interior", "e", "capital"}
+	fmt.Println(ss)
+	sort.Strings(ss)
+	fmt.Println(ss)
+	ss1 := []int{1, 4, 7, 9, 20, 2, 3, 4, 5, 7, 101, 101, 2031013, 2012, 12, 1201, 21, 2012, 03, 303013, 1301}
+	fmt.Println(ss1)
+	sort.Ints(ss1)
+	fmt.Println(ss1)
+}
+
+//sort costumizado
+//implementando a interface sort e conseguindo reutilizar sort atraves de uma implementação diferente
+
+type carro struct {
+	Nome     string
+	Potencia int
+	Consumo  int
+}
+
+// implementação por Potencia
+type OrdenarPorPotencia []carro
+
+func (x OrdenarPorPotencia) Len() int {
+	return len(x)
+}
+func (x OrdenarPorPotencia) Less(i, j int) bool {
+	return x[i].Potencia < x[j].Potencia
+}
+
+func (x OrdenarPorPotencia) Swap(i, j int) { x[i], x[j] = x[j], x[i] }
+
+// implementação por Consumo
+type OrdenarPorConsumo []carro
+
+func (x OrdenarPorConsumo) Len() int {
+	return len(x)
+}
+func (x OrdenarPorConsumo) Less(i, j int) bool {
+	return x[i].Consumo < x[j].Consumo
+}
+
+func (x OrdenarPorConsumo) Swap(i, j int) { x[i], x[j] = x[j], x[i] }
+
+// implementação por Lucro
+type OrdenarporLucroProDonoDoPosto []carro
+
+func (x OrdenarporLucroProDonoDoPosto) Len() int {
+	return len(x)
+}
+func (x OrdenarporLucroProDonoDoPosto) Less(i, j int) bool {
+	return x[i].Consumo < x[j].Consumo
+}
+
+func (x OrdenarporLucroProDonoDoPosto) Swap(i, j int) { x[i], x[j] = x[j], x[i] }
+
+func Len() int {
+	return 0
+}
+func exemplo06() {
+	carros := []carro{
+		carro{"Palio", 400, 4},
+		carro{"Chevet", 200, 1},
+		carro{"Mofi", 300, 3},
+		carro{"Tatror", 560, 1},
+	}
+
+	fmt.Println("Inicial:\n", carros)
+	sort.Sort(OrdenarPorPotencia(carros))
+	fmt.Println("Potência:\n", carros)
+
+	sort.Sort(OrdenarPorConsumo(carros))
+	fmt.Println("Consumo:\n", carros)
+
+	sort.Sort(OrdenarporLucroProDonoDoPosto(carros))
+	fmt.Println("Lucro:\n", carros)
+}
+
+// usando bcrypt
+func exemplo07() {
+	senha := "22Outubro1991"
+	senhaerrada := "21Outubro1991"
+	sb, err := bcrypt.GenerateFromPassword([]byte(senha), 10)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(string(sb))
+
+	err = bcrypt.CompareHashAndPassword(sb, []byte(senhaerrada))
+
+	if err != nil {
+		fmt.Println("Senha errada....")
+	} else {
+		fmt.Println("A senha está Correta...")
+	}
+	err = bcrypt.CompareHashAndPassword(sb, []byte(senha))
+
+	if err != nil {
+		fmt.Println("Senha errada....")
+	} else {
+		fmt.Println("A senha está Correta...")
+	}
 }
